5818b087345e1d1c08d90ea821a01419852258dc
Tokaallah <tokallah.92@gmail.com>
2023-05-09 13:32:13 +0000

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def uppercase(s):
+    for c in s:
+        if ord('a') <= ord(c) <= ord('z'):
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end='')
+    print('')
