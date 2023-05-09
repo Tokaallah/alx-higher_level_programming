e318ad1a07177e7a717ca3aed33029f084234266
Tokaallah <tokallah.92@gmail.com>
2023-05-09 13:19:17 +0000

    Fix error

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 
 for i in range(10):
     for j in range(i+1, 10):
-        for k in range(j+1, 10);
+        for k in range(j+1, 10):
             print("{:d}{:d}{:d}".format(i, j, k), end='')
             if i < 7 or (i == 7 and j < 8) or (i == 8 and j == 8 and k < 9):
                 print(", ", end='')
