2fcdb4e6090156717909d50b3874ff16e7d5e392
Tokaallah <tokallah.92@gmail.com>
2023-05-11 09:55:00 +0000

    fix error

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
 
-from calculator_1 import add, sub, mul, div
-
-a = 10
-b = 5
-
-sum = add(a, b)
-diff = sub(a, b)
-prod = mul(a, b)
-quot = div(a, b)
-
-print("{} + {} = {}".format(a, b, sum))
-print("{} - {} = {}".format(a, b, diff))
-print("{} * {} = {}".format(a, b, prod))
-print("{} / {} = {}".format(a, b, quot))
-
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
