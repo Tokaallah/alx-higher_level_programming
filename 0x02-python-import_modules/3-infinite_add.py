a999c9472ca37dad1b1e27e5f3bca58e693a3742
Tokaallah <tokallah.92@gmail.com>
2023-05-11 10:05:47 +0000

    fix error

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
 
-import sys
+	import sys
 
-args = sys.argv[1:]
-result = sum(int(arg) for arg in args)
+	args = sys.argv[1:]
+	result = sum(int(arg) for arg in args)
 
-print(result)
+	print(result)
