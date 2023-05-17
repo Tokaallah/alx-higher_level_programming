861db37b0acb939d1cad48cb2d867d3a7df524a8
Tokaallah <tokallah.92@gmail.com>
2023-05-17 07:56:04 +0000

    square matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,13 @@
+def square_matrix_simple(matrix=[]):
+    # Create a new matrix with the same size as the input matrix
+    result = [[0] * len(row) for row in matrix]
+    
+    # Iterate over each element in the input matrix
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            # Compute the square value of the element
+            squared_value = matrix[i][j] ** 2
+            # Assign the squared value to the corresponding element in the result matrix
+            result[i][j] = squared_value
+    
+    return result
