0c4ef27fed1d1bdc3053e8f20d965511b986c0ae
Tokaallah <tokallah.92@gmail.com>
2023-05-21 10:32:47 +0000

    fix error

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
-for char_code in range(90, 64, -1):
-    print(chr(char_code), end='')
-    char_code =+ 6
-    print(chr(char_code), end='')
+alpha = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - alpha)), end="")
+    alpha = 32 if alpha == 0 else 0
