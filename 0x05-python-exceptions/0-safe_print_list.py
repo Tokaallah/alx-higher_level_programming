ed2ca26ade70e72b087bbd7f88d848c03f7573a2
Tokaallah <tokallah.92@gmail.com>
2023-05-29 09:13:57 +0000

    fix errors

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
 
     try:
         for i in range(x):
-            print(my_list[i], end=" ")
+            print(my_list[i], end="")
             count += 1
     except IndexError:
         pass  # Ignore index errors when x is larger than the list length
