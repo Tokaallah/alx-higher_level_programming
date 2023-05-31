e0ff3bc3d05630fd98b628e65cc32a75a7833ffb
Tokaallah <tokallah.92@gmail.com>
2023-05-31 03:09:03 +0000

    fix errors

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
+"""
+This module defines the Square class.
+"""
+
+
+class Square:
+    """
+    This class represents a square.
+    """
+    pass
 
