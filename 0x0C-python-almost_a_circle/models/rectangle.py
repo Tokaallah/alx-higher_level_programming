1df1b59c8da0ce9a7dff0a8ff262200a6941b399
Tokaallah <tokallah.92@gmail.com>
2023-06-17 05:53:52 +0000

    [Feature]: rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,76 @@
+
+#!/usr/bin/python3
+
+"""
+This module contains the Rectangle class.
+"""
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    Rectangle class, inherits from Base.
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initialize the Rectangle instance.
+
+        Args:
+            width (int): Width of the rectangle.
+            height (int): Height of the rectangle.
+            x (int): x-coordinate of the rectangle's position.
+            y (int): y-coordinate of the rectangle's position.
+            id (int): Unique identifier of the rectangle.
+
+        Notes:
+            Calls the super class with id to utilize the logic of the __init__ method in the Base class.
+            Assigns each argument width, height, x, and y to the corresponding attribute.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Getter for width attribute."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for width attribute."""
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for height attribute."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for height attribute."""
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter for x attribute."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setter for x attribute."""
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter for y attribute."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter for y attribute."""
+        self.__y = value
+
