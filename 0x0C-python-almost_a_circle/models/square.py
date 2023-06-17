edbf1e3e67a50c252e58e0606198a908c3ac9df8
Tokaallah <tokallah.92@gmail.com>
2023-06-17 06:49:10 +0000

    [Update]: update methode

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -3,26 +3,39 @@
 
 from models.rectangle import Rectangle
 
-
 class Square(Rectangle):
     """ Represents a square """
 
     def __init__(self, size, x=0, y=0, id=None):
-        """ Initializes a square instance """
+        """ Initializes a Square instance """
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """ Retrieves the size """
+        """ Getter for the size attribute """
         return self.width
 
     @size.setter
     def size(self, value):
-        """ Sets the size """
+        """ Setter for the size attribute """
         self.width = value
         self.height = value
 
     def __str__(self):
-        """ Returns a string representation of the square """
+        """ Returns a string representation of the Square instance """
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+
+    def update(self, *args, **kwargs):
+        """ Updates the attributes of the Square instance """
+        if args:
+            attrs = ['id', 'size', 'x', 'y']
+            for attr, val in zip(attrs, args):
+                setattr(self, attr, val)
+        else:
+            for key, val in kwargs.items():
+                setattr(self, key, val)
+
+    def to_dictionary(self):
+        """ Returns a dictionary representation of the Square instance """
+        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
 
