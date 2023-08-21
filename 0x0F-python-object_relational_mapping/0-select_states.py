673e045227cb5759ba06e8f50707c14cee6d69eb
Tokaallah <tokallah.92@gmail.com>
2023-08-21 07:40:18 +0000

    fix error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
+"""
+here we are going to connect to a database and realize a
+query
+
+"""
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id")
-    [print(state) for state in cur.fetchall()]
+if __name__ == "__main__":
+    if (len(sys.argv) == 4):
+        db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                             passwd=sys.argv[2], db=sys.argv[3],
+                             charset="utf8")
+        query = db.cursor()
+        query.execute("SELECT * FROM states ORDER BY id ASC")
+        out = query.fetchall()
+        for i in out:
+            print(i)
+        query.close()
+        db.close()
