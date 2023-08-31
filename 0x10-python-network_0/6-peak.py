9a82a4f928726b722f2f88c04b0235542a1c2229
Tokaallah <tokallah.92@gmail.com>
2023-08-31 18:27:43 +0000

    fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
-# finds peak
+""" find the peak
+"""
 
 
-def search(lo, h, ints):
-    mid = (lo + h) // 2
-    if lo == h:
-        return ints[h]
-    if ints[mid] < ints[mid + 1]:
-        return(search(mid + 1, h, ints))
-    return(search(lo, mid, ints))
+def recursion(begin, last, l):
+    """ search
+    """
+    mid = (begin + last)
+
+    if (mid % 2 == 0):
+        m = int(mid / 2)
+    else:
+        m = int((mid - 1) / 2)
+    if (m == 0 or l[m] >= l[m - 1]) and (m == last - 1 or l[m] >= l[m + 1]):
+        return (l[m])
+    elif (m > 0 and l[m] < l[m - 1]):
+        return (recursion(begin, m, l))
+    elif (m < last - 1 and l[m] < l[m + 1]):
+        return (recursion(m, last, l))
+    else:
+        return(None)
 
 
 def find_peak(list_of_integers):
+    """ function that get the peak from a list
+    """
     if not list_of_integers:
-        return
-    return(search(0, len(list_of_integers) - 1, list_of_integers))
+        return (None)
+
+    l = list_of_integers
+    return (recursion(0, len(l), l))
